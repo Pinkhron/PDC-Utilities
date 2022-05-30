f8dfcea01cad0504f03dde8760b34c0406a59954
Pinkhron <pinkhronstudios@gmail.com>
2022-05-29 23:13:36 -0700

    Release 1.2.1

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ async def on_message(message):
         ping_embed = discord.Embed(title=f"PDC Utilities v{v}", url="https://github.com/Pinkhron/PDC-Utilities",
                                    description="PDC Utilities is an open-source bot made by <@597178180176052234> to "
                                                "manage PDC easier. PDC Utilities was made with Python & Discord.py\n\n"
-                                               f"- [Discord.py]({https://github.com/Rapptz/discord.py}) | v{discord.__version__}"
+                                               f"- [Discord.py](https://github.com/Rapptz/discord.py) | v{discord.__version__}"
                                                f"- [Python](https://python.org) | v{platform.python_version()}")
         ping_embed.set_thumbnail(url=config["bot"]["icons"]["confetti"])
         ping_embed.set_footer(text="Made with \u2764\uFE0F by Pinkhron | \u00a9 PDC Utilities 2022",
