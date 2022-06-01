114a92a8afc565f5df84bd8a01406a22a33b56ab
Pinkhron <pinkhronstudios@gmail.com>
2022-05-31 18:06:25 -0700

    Release 1.8.2

--- main.py
+++ main.py
@@ -155,6 +155,8 @@ async def _numgen(ctx, num1: int, num2: int):
     if num1 > num2:
         ctx.reply("❌ First number cannot be higher than the second number.")
         return
+    elif (num1 > 1e12) or (num2 > 1e12):  # 1 trillion
+        ctx.reply("❌ Numbers cannot be over a trillion")
 
     _result = discord.Embed(title='🎲 Number Generated!', description=f'```{random.randint(num1, num2)}```')
     _result.set_footer(text=_footer, icon_url=logo2)
