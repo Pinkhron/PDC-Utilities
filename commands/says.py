2f5cd2bab8c78cb672b782ba4daccabdf908ef05
Pinkhron <pinkhronstudios@gmail.com>
2022-06-09 05:43:25 -0700

    Release 2.3.0b

--- commands/says.py
+++ commands/says.py
@@ -1,17 +1,27 @@
 from discord import app_commands
 from discord.ext import commands
+from discord.utils import get
 import discord
 
 from data import Data
 
 # Embeds
 
+_readme = 'https://discord.com/channels/966934902878646323/984309064244817931/984365351460548648'
+
 _confirmation = discord.Embed(title='Are you sure?',
                               description='Are you sure you want to start a new says game? This confirmation is here to'
                                           ' make sure this wasn\'t a mistake. **Starting a game for no reason will get '
                                           'you blacklisted from using Says as host for a week!**',
                               color=0x00FF00)
-_confirmation.set_footer(text=Data.FOOTER, icon_url=Data.LOGO_BOT)
+
+_new = discord.Embed(title='Welcome to Says!', description='The host will be in full control of the game with host '
+                                                           'slash commands. To invite players to the game use '
+                                                           '`/says invite <usr>`, it will send a confirmation DM to '
+                                                           'the user to whom you (host) invited. To read more on how '
+                                                           'to play please check the pins or '
+                                                           f'[click here]({_readme}).')
+_new.set_footer(text=Data.FOOTER, icon_url=Data.LOGO_BOT)
 
 
 class Confirm(discord.ui.View):
@@ -21,8 +31,9 @@ class Confirm(discord.ui.View):
 
     @discord.ui.button(emoji='✅', label='Confirm', style=discord.ButtonStyle.green)
     async def _accept(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message(embed=discord.Embed(description=f'{Data.EMOTE_LOAD} '
-                                                                                'Starting a new game..'))
+        await interaction.response.send_message(content=f'<@!{interaction.user.id}>',
+                                                embed=discord.Embed(description=f'{Data.EMOTE_LOAD} Starting a new game'
+                                                                                '..'))
         self.value = True
         self.stop()
 
@@ -38,11 +49,11 @@ class Says(commands.GroupCog, name='says'):
         self.bot = bot
         super().__init__()
 
-    # Slash commands
+    # Slash commands (CHANGE ROLE ON RELEASE)
 
     @app_commands.command(name='start', description='Starts a new game of Says')
     @app_commands.checks.cooldown(1, 30, key=lambda i: i.user.id)
-    @app_commands.checks.has_role(Data.ROLE_MEMBER)
+    @app_commands.checks.has_role(Data.ROLE_ORGANIZER)
     async def _start(self, interaction: discord.Interaction):
         view = Confirm()
         await interaction.response.send_message(embed=_confirmation, view=view, ephemeral=True)
@@ -52,8 +63,24 @@ class Says(commands.GroupCog, name='says'):
             await interaction.response.send_message(content=':clock3: Timed out', ephemeral=True)
         elif view.value:
             await interaction.response.send_message(content=':eyes: coming soon')
+
+            host = get(interaction.guild.roles, name="SAYS HOST")  # Give host role
+            await interaction.user.add_roles(host)
+
+            channel = self.bot.get_channel(Data.TXT_SAYS)
+            await channel.send(content=f'**:game_die: A new game of Says has begun!** Host: <@!{interaction.user.id}>',
+                               embed=_new)
         else:
             return
+
+    # Error handling
+
+    @_start.error
+    async def err(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, app_commands.CommandOnCooldown):  # Command cooldown
+            await interaction.response.send_message(str(error), ephemeral=True)
+        elif isinstance(error, app_commands.MissingPermissions):  # Missing perms
+            await interaction.response.send_message(str(error), ephemeral=True)
 
 
 async def setup(bot: commands.Bot):
