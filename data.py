15827c2d594bfd8ff0411efb46774fbe7edb3a38
Pinkhron <pinkhronstudios@gmail.com>
2022-07-01 00:53:13 -0700

    Release v3.0.0a

--- data.py
+++ data.py
@@ -1,30 +1,16 @@
 class Data:
-    # Guild data
+    # Bot data
+    NAME = 'PDC Utilities'
+    VERSION = '3.0.0a'
+    ICON = 'https://pinkhron.s3.amazonaws.com/PDC/icons/bot.png'
+    OWNER_ID = 597178180176052234
+    MAIN_COLOR = 0x6f0dd1
 
-    GUILD_ID = 966934902878646323
-    TXT_SAYS = 984309064244817931
-    TXT_BOTS = 966958813565550592
+    # Text channels
+    TXT_GENERAL = 966934903381983324
 
-    ROLE_ORGANIZER = 966936260285448202
-    ROLE_SAYS_HOST = 984363103955333150
-    ROLE_SAYS_COHOST = 984713330440667156
-    ROLE_SAYS_PARTICIPANT = 984363361141678130
+    # Emojis
+    EMOTE_MEMBER = '<PDC_Member:992327295572377651>'
+
+    # Roles
     ROLE_MEMBER = 966940726074175498
-
-    CLR_PDC = 0x1C2482
-    CLR_BLURPLE = 0x7289da
-
-    # Bot assets
-
-    VERSION = "2.5.0"
-    EMOTE_LOAD = '<a:PDC_Loading:980936150065750036>'
-    LOGO_BOT = "https://pinkhron.s3.amazonaws.com/PDC/icons/bot.png"
-    LOGO_DEFAULT = "https://pinkhron.s3.amazonaws.com/PDC/icons/default.png"
-
-    # Bot responses
-
-    SHIP_LOAD = ["Reading diaries", "Reading divorce papers", "Checking finances", "Scrolling through DMs"]
-    BALL_RESPONSE = ["Yes!", "Sure.", "Ok", "Positive", "Hell yeah", "Is that even a no",
-                    "No.", "Nah", "Hell no.", "In your dreams", "No chance", "Negative",
-                    "Idk", "hmm", "Ask again later, I'm too lazy rn", "Really?", "HAHAHAHAHAHA"]
-    FOOTER = "Made with ❤️ by Pinkhron | © PDC Utilities 2022"
--- main.py
+++ main.py
@@ -1,140 +1,113 @@
 import os
+import random
 import platform
 from datetime import datetime
 
-from discord.ext.commands import is_owner
+import discord
 from discord.ext import commands
 from discord.utils import get
-import discord
 
 from dotenv import load_dotenv
 from data import Data
 
-# Load ENV
+# ENV & File settings
 
 load_dotenv()
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv('DISCORD')
 
-# Initialize bot
+random.seed(os.urandom(64))
+
+# Initialize client
+
+print(f'Starting PDC Utilities v{Data.VERSION}..')
 
 intents = discord.Intents.default()
 intents.guilds = True
-intents.members = True
-intents.guild_messages = True
+intents.messages = True
 intents.message_content = True
+intents.members = True
 
-bot = commands.Bot(command_prefix='>', intents=intents, owner_id=597178180176052234)
+bot = commands.Bot(command_prefix='$', intents=intents, owner_id=Data.OWNER_ID)
 
 
-class NewConfirm(discord.ui.View):
-    def __init__(self):
-        super().__init__()
-        self.value = None
-
-    @discord.ui.button(emoji='✅', label='Confirm', style=discord.ButtonStyle.green)
-    async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message(content='working..')
-        self.value = True
-        self.stop()
-
-    @discord.ui.button(emoji='❌', label='Deny', style=discord.ButtonStyle.grey)
-    async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message(content='working..')
-        self.value = False
-        self.stop()
-
+# Bot
 
 @bot.event
 async def on_ready():
-    print('Successfully logged in as {0.user}'.format(bot))
-
-    if __name__ == '__main__':  # File handling
-        for cmd in os.listdir('./commands'):  # Slash commands
-            if cmd.endswith('.py'):
-                await bot.load_extension(f'commands.{cmd[:-3]}')
+    print(f'Successfully logged in as {bot.user}')
 
 
 @bot.event
 async def on_message(message):
-    if message.author.bot:  # Doesn't respond to bots
+    if message.author.bot:
         return
 
-    if message.content.lower() == 'pdc':  # Respond to PDC
-        await message.reply('is awesome', mention_author=False)
+    if message.content.lower() == 'pdc':
+        await message.reply(content=f'is {random.choice(["epic", "awesome", "amazing", "the best", "on fire :fire:"])}')
 
-    if bot.user.mentioned_in(message) and len(bot.user.mention) == len(message.content):  # Mention embed
-        version = Data.VERSION
-
-        _mention = discord.Embed(title=f'\U0001F44B Hey, {message.author.name}!',
-                                 description='PDC Utilities is an open-source bot made by <@597178180176052234> to '
-                                             'make managing PDC easy & to make PDC fun! Click '
-                                             '[**here**](https://discord.com/channels/966934902878646323/966958813565550592/984320243830755349) '
-                                             f'or check pins in <#{Data.TXT_BOTS}> for a list of commands. '
-                                             'PDC Utilities was made with Python & Discord.py\n\n'
-                                             f'- [Python](https://python.org) | v{platform.python_version()}\n'
-                                             f'- [Discord.py](https://github.com/Rapptz/discord.py) | v{discord.__version__}\n'
-                                             f'- [PDC Utilities](https://github.com/Pinkhron/PDC-Utilities) | v{version}\n'
-                                             f'[PinkhronNetwork Status](https://status.pinkhron.net)\n\n'
-                                             'Thank you for joining PDC!')
-        _mention.set_thumbnail(url=Data.LOGO_DEFAULT)
-        _mention.set_footer(text=Data.FOOTER, icon_url=Data.LOGO_BOT)
-
-        await message.reply(embed=_mention)
+    if bot.user.mentioned_in(message) and len(bot.user.mention) == len(message.content):
+        await message.reply(embed=discord.Embed(
+            title=f':wave: Hey, {message.author.name}',
+            color=Data.MAIN_COLOR,
+            timestamp=datetime.now(),
+            description='PDC Utilities is an open-source bot using the MIT Licence and is actively developed by Pinkhron'
+        ).add_field(
+            name='Versioning:',
+            value=f'- [**Python**](https://python.org) | v{platform.python_version()}\n'
+                  f'- [**PDC Utilities**](https://github.com/Pinkhron/PDC-Utilities) | v{Data.VERSION}\n'
+                  f'- [**Discord.py**](https://github.com/Rapptz/discord.py) | v{discord.__version__}'
+        ).set_footer(text=Data.NAME, icon_url=Data.ICON))
 
     await bot.process_commands(message)
 
 
 @bot.event
 async def on_member_join(member):
-    general = bot.get_channel(966934903381983324)
-    view = NewConfirm()
+    # Button
+    class Confirmation(discord.ui.View):
+        def __init__(self):
+            super().__init__()
+            self.value = None
 
-    await general.send(embed=discord.Embed(
-        title=':tickets: New Member',
-        description=f'<@!{member.id}> has joined PDC. If you\'ve invited them please press accept. Accepting will grant '
-                    f'them access to all of the member channels. If you invited someone by mistake/leaked invite press '
-                    f'decline. Declining will kick them out of the server. **Inviting random people and granting them '
-                    f'access into the server will get your account blacklisted from using this feature until further '
-                    f'notice. All actions are logged.** You will have `3` minutes to respond before they are auto-kicked.',
-        timestamp=datetime.now()
-    ).set_footer(text=Data.FOOTER, icon_url=Data.LOGO_BOT), view=view)
+        @discord.ui.button(emoji='✅', label='Confirm', style=discord.ButtonStyle.green)
+        async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
+            await interaction.response.send_message(content=f'<@!{interaction.user.id}> Confirming..')
+            self.value = 1
+            self.stop()
+
+        @discord.ui.button(emoji='❌', label='Deny', style=discord.ButtonStyle.gray)
+        async def deny(self, interaction: discord.Interaction, button: discord.ui.Button):
+            await interaction.response.send_message(content=f'<@!{interaction.user.id}> Kicking user..')
+            self.value = 0
+            self.stop()
+
+        @discord.ui.button(emoji='🕒', label='Void', style=discord.ButtonStyle.red)
+        async def void(self, interaction: discord.Interaction, button: discord.ui.Button):
+            await interaction.response.send_message(content=f'<@!{interaction.user.id}> Removing kick timer..')
+            self.value = 2
+            self.stop()
+
+    # Response
+    general = bot.get_channel(Data.TXT_GENERAL)
+    view = Confirmation()
+
+    message = await general.send(embed=discord.Embed(
+        title=f'{Data.EMOTE_MEMBER} A new member has joined PDC!',
+        timestamp=datetime.now(),
+        description='placeholder'
+    ).set_footer(text=Data.NAME, icon_url=Data.ICON), view=view)
+
     await view.wait()
-
     if view.value is None:
-        await member.kick()
-        await general.send(content=f':clock3: Kicked <@!{member.id}> due to button timeout.')
-    elif view.value is True:
-        guild = bot.get_guild(966934902878646323)
-        role = get(guild.roles, id=Data.ROLE_MEMBER)
-        await member.add_roles(role)
-        await general.send(content=f':white_check_mark: Granted <@!{member.id}> access into the server.')
-    else:
-        await member.kick()
-        await general.send(content=f'Kicked <@!{member.id}>')
-
-
-# Regular commands
-
-@bot.command(name='sync')  # Sync (/) commands
-@is_owner()
-async def _sync(ctx):
-    await ctx.send('Syncing...')
-
-    try:
-        await bot.tree.sync(guild=discord.Object(id=Data.GUILD_ID))
-        await ctx.send('Success!')
-    except discord.errors.Forbidden:
-        await ctx.send('Error')
-
-
-@bot.command(name='ping')
-async def _ping(ctx):
-    _pingEmbed = discord.Embed(title=':ping_pong: Pong!', description=f'```{round(bot.latency * 1000)}ms```')
-    _pingEmbed.set_footer(text=Data.FOOTER, icon_url=Data.LOGO_BOT)
-
-    await ctx.reply(embed=_pingEmbed, mention_author=False)
-
-# Run bot
-
+        await member.kick(reason='Failed to be confirmed upon join')
+        await message.edit(content=f'<@!{member.id}> has been auto-kicked due to button timeout', suppress=True)
+    elif view.value == 1:
+        await member.add_roles(get(member.guild.roles, id=Data.ROLE_MEMBER))
+        await general.send(content=f'Successfully granted <@!{member.id}> access into the server')
+    elif view.value == 0:
+        await member.kick(reason='Kicked by a PDC member upon join')
+        await general.send(content=f'Successfully kicked new member <@!{member.id}> upon user request')
+    elif view.value == 2:
+        await general.send(content=f'Successfully voided <@!{member.id}>\'s auto-kick timer')
 
 bot.run(TOKEN)
